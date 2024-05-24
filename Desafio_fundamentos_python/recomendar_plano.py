10ccb2e031bf34fa7198dc4e2820d7d371871682
kayo <kayohramos@gmail.com>
2024-05-24 14:36:32 -0300

    desafio lista de equipamentos e validação de numero de telefone

--- Desafio_fundamentos_python/recomendar_plano.py
+++ Desafio_fundamentos_python/recomendar_plano.py
@@ -7,6 +7,8 @@ def recomendar_plano(consumo):
         return "Plano Premium Fibra - 300Mbps"
 
 # Solicita ao usuário que insira o consumo médio mensal de dados:
+
+
 consumo = float(input())
 # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
 print(recomendar_plano(consumo))
